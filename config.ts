64efe067bd6d6ba1cff8f7fe3e02d4b0e92cbed5
Timmy Jose <zoltan.jose@gmail.com>
2024-06-06 22:18:37 +0530

    feat: persistence enabled fore firebase/firestore (doesn't work on RN)

--- config.ts
+++ config.ts
@@ -1,10 +1,8 @@
 import { FIREBASE_CONFIG } from './constants'
 import { initializeApp } from 'firebase/app'
-import { getFirestore, initializeFirestore } from 'firebase/firestore'
+import { initializeFirestore, persistentLocalCache } from 'firebase/firestore'
 
 const firebaseConfig = JSON.parse(FIREBASE_CONFIG)
-
 export const firebaseApp = initializeApp(firebaseConfig)
 
-// export const firebaseDb = initializeFirestore(firebaseApp, {localCache: memoryLocalCache({})})
-export const firebaseDb = getFirestore(firebaseApp)
+export const firebaseDb = initializeFirestore(firebaseApp, {localCache: persistentLocalCache({})})
